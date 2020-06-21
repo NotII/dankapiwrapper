1c4c2320c6fa4dcdee7a433d6d0ec531c24da8b9
mitch <expertemail102@gmail.com>
2020-06-21 16:32:38 +0100

    Fixed youtube & changed example

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const DankAPI = require("./lib/client");
 const client = new DankAPI("token");
 const fs = require("fs");
 
-client.abandon("fuck you dad").then(img => {
+client.youtube("https://image.shutterstock.com/image-photo/white-ragdoll-cat-pink-coat-600w-1741372145.jpg", "mitch", "testing !!").then(img => {
     img.pipe(fs.createWriteStream("./img.jpg"))
 })
